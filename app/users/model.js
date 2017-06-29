52a5ef0f10e1da486d5b8dd29a480e1ad7ac5706
wudtichaikarun <595159090004@dpu.ac.th>
2017-06-30 05:45:25 +0700

    yarn fix cannot run server

--- app/users/model.js
+++ app/users/model.js
@@ -2,11 +2,11 @@ import bcrypt from 'bcrypt'
 import Model from '../model'
 
 const Users = {
-   ...Model,
-   key: 'users',
-   permittedAttrs: ['email'],
+  ...Model,
+  key: 'users',
+  permittedAttrs: ['email'],
 
-   createnewUser(email, password){
+  createnewUser(email, password){
     return new Promise((resolve, reject) => {
       bcrypt.hash(password, 12, (err, hash) => {
         if(err) return reject(err)
@@ -21,8 +21,8 @@ const Users = {
         
          this.setCollection([...collection, user])
          return resolve(user)
-       })
-     })
-   }
+      })
+    })
+  }
 }
 export default Users
